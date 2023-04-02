167851fa8237599489afb59c98cc49b58c1eba03
Nishant Tiwari <87887576+Nishant3231@users.noreply.github.com>
2023-04-02 11:54:55 +0530

    Update Navbar.js
    Added the basic layout of navbar

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Navbar() {
+function NavBar() {
   return (
-    <div>Design navbar here</div>
-  )
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">KrishiVision</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/about'>About</Nav.Link>
+            <Nav.Link href='/community'>Community</Nav.Link>
+            
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
+
+export default NavBar;
