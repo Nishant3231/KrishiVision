670529ddccaa8039954ea051a0fff9aaddbcf4ad
Nishant Tiwari <87887576+Nishant3231@users.noreply.github.com>
2023-04-16 23:32:15 +0530

    Update LandingPage.js

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Image from 'next/image'
 import Button from 'react-bootstrap/Button';
@@ -8,41 +10,44 @@ import CardGroup from 'react-bootstrap/CardGroup';
 
 function LandingPageCards() {
   return (  
-     <CardGroup>
-      <Card style={{ width: 500 }}>
+    <div className={styles.main} >
+      
+     <CardGroup className={styles.cardgrpdiv}>
+      <Card className={styles.indicard}>
         <Card.Img variant="top" src="/FertilizerImg.jpg" width={200}
-      height={300} alt="Card image" />
+      height={300} className={styles.indiimg} alt="Card image" />
         <Card.Body>
           <Card.Title>Fertilizer Suggestion</Card.Title>
           <Card.Text>
           This page will take you to model that will suggest you which fertilizer to use based on your provided information.
           </Card.Text>
-          <Button variant="primary" Link href='/fertilizer' >Click here</Button>
+          <Button variant="primary" className={styles.butt} Link href='/fertilizer' >Click here</Button>
         </Card.Body>
       </Card>
-      <Card>
+      <Card className={styles.indicard}>
         <Card.Img variant="top" src="/CropImg.jpg" width={200}
-      height={300} alt="Card image"/>
+      height={300} className={styles.indiimg} alt="Card image"/>
         <Card.Body>
-          <Card.Title>Crop Prediction</Card.Title>
+          <Card.Title>Crop Recommemdation</Card.Title>
           <Card.Text>
           This page will take you to model that will suggest you which Crop to grow based on your provided information.
           </Card.Text>
-          <Button variant="primary" Link href='/crop' >Click here</Button>
+          <Button variant="primary" Link href='/crop' className={styles.butt} >Click here</Button>
         </Card.Body>
       </Card>
-      <Card>
+      <Card className={styles.indicard}>
         <Card.Img variant="top" src="/diseaseImg.jpg" width={200}
-      height={300} alt="Card image"/>
+      height={300} className={styles.indiimg} alt="Card image"/>
         <Card.Body>
           <Card.Title>Disease Prediction</Card.Title>
           <Card.Text>
           This page will take you to model that will predict the crop disease based on your provided information.
           </Card.Text>
-          <Button variant="primary" Link href='/disease' >Click here</Button>
+          <Button variant="primary" Link href='/disease' className={styles.butt} >Click here</Button>
         </Card.Body>
       </Card>
     </CardGroup>
+    </div>
   );
 }
 export default LandingPageCards;
